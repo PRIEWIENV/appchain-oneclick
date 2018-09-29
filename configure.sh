4737780bb9915c898aa1045ac8e9fbc417ca0073
PRIEWIENV <st.yeah@gmail.com>
2018-09-29 16:02:29 +0800

    :bug: fix: the macos environment varibles

--- configure.sh
+++ configure.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/env bash
 
+if [[ `uname` == 'Darwin' ]]
+then
+    cp /etc/localtime $PWD/localtime
+    LOCALTIME_PATH="$PWD/localtime"
+else
+    LOCALTIME_PATH="/etc/localtime"
+fi
+
+USER_ID=`id -u $USER`
+
+echo > .env
+
+echo "USER_ID=${USER_ID}" >> .env
+echo "LOCALTIME_PATH=${LOCALTIME_PATH}" >> .env
+
 cp re-birth/.env.local re-birth/re-birth/
